ef3c31ee51eba65da2f2367f375ec8a3ec853d5a
cart <termik@inbox.ru>
2019-02-24 14:34:19 +0700

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Hazel applications
+
+#include "Hazel/Application.h"
+
+//------Hazel Entry-Point -----------------------
+#include "Hazel/EntryPoint.h"
+//-----------------------------------------------
