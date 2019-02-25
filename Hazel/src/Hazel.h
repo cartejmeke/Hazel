9afb78666cd6043e158681ed4b2fce305fc36249
cart <termik@inbox.ru>
2019-02-25 15:38:51 +0700

    Log system and premake.

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,6 +3,7 @@
 // For use by Hazel applications
 
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 //------Hazel Entry-Point -----------------------
 #include "Hazel/EntryPoint.h"
---
+++ Hazel/src/Hazel/Log.cpp
@@ -0,0 +1,20 @@
+#include "Log.h"
+
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace Hazel {
+
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_ÑlientLogger;
+
+	void Log::Init()
+	{
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+		s_CoreLogger = spdlog::stdout_color_mt("HAZEL");
+		s_CoreLogger->set_level(spdlog::level::trace);
+
+		s_ÑlientLogger = spdlog::stdout_color_mt("APP");
+		s_ÑlientLogger->set_level(spdlog::level::trace);
+	}
+
+}
